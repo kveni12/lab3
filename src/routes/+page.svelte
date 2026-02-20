cba7ce1efe89852fd237046c457b18fca2e7212f
Krishna <kveni@mit.edu>
2026-02-19 22:16:18 -0500

    uploaded website stuff from labs 1 and 2

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,10 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<nav> 
+        <a class="current" href=".">Home</a>
+        <a href="projects">Projects</a>
+        <a href="contact">Contact</a>
+        <a href="resume">Resume</a>
+        <li><a href="https://github.com/kveni12" target="_blank">Github</a></li>
+    </nav>
+    <h1> Krishna Parvataneni</h1>
+    <p> Hi, I’m Krishna Parvataneni. I’m an undergraduate at MIT studying Artificial Intelligence and Decision-Making, with interests in machine learning, medical imaging, and healthcare innovation. I enjoy building tools that bridge technical rigor with real-world clinical impact.</p>
+    <img src="images/krishna.jpg" alt="a picture of me!">
--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,3 +1,29 @@
 <svelte:head>
   <title>Contact me</title>
 </svelte:head>
+<nav> 
+        <a class="home" href="../">Home</a>
+        <a href="../projects">Projects</a>
+        <a class="current" href="../contact">Contact</a>
+        <a href="../resume">Resume</a>
+        <a href="https://github.com/kveni12" target="_blank">Github</a>
+    </nav>
+    <h1>Contact Me</h1>
+    <form action="mailto:kveni@mit.edu" method="GET">
+        <label>
+            Email:
+            <input name="Email" type="email">
+        </label>
+
+        <label>
+            Subject:
+            <input name="subject">
+        </label>
+
+        <label>
+            Message:
+            <textarea name="body" rows="10" cols="30"></textarea>
+        </label>
+
+        <button type="submit">Submit</button>
+    </form>
