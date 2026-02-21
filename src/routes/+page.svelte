586afc2674711c56f7bfcbdfdd41a6cc13a1145a
Krishna <kveni@mit.edu>
2026-02-21 09:32:15 -0500

    reformatted projects + added 3 projects to home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,7 @@
+<script>
+  import projects from "$lib/projects.json";
+  import Project from "$lib/Project.svelte";
+</script>
 <nav> 
         <a class="current" href=".">Home</a>
         <a href="projects">Projects</a>
@@ -8,3 +12,9 @@
     <h1> Krishna Parvataneni</h1>
     <p> Hi, I’m Krishna Parvataneni. I’m an undergraduate at MIT studying Artificial Intelligence and Decision-Making, with interests in machine learning, medical imaging, and healthcare innovation. I enjoy building tools that bridge technical rigor with real-world clinical impact.</p>
     <img src="images/krishna.jpg" alt="a picture of me!">
+    <h2> Some of my projects:</h2>
+    <div class="projects highlights">
+    {#each projects.slice(0, 3) as p}
+        <Project data={p} />
+    {/each}
+    </div>
