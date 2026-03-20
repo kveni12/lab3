1e7fafb8189f3669547d8e233569e114c55dcb80
Krishna <kveni@mit.edu>
2026-03-19 20:15:21 -0400

    lab 7 - 1.4

--- src/routes/meta/+page.svelte
+++ src/routes/meta/+page.svelte
@@ -8,7 +8,21 @@ import BarHorizontal from '$lib/BarHorizontal.svelte';
 let locData = [];
 let languageData = [];
 let commits = [];
+let width = 1000, height = 600;
 
+// scales
+$: [minDate, maxDate] = d3.extent(commits.map(d => d.date));
+$: maxDatePlusOne = new Date(maxDate);
+$: maxDatePlusOne.setDate(maxDatePlusOne.getDate() + 1);
+
+$: xScale = d3.scaleTime()
+              .domain([minDate, maxDatePlusOne])
+              .range([0, width])
+              .nice();
+
+$: yScale = d3.scaleLinear()
+              .domain([24, 0])
+              .range([height, 0]);
 onMount(async () => {
 
     locData = await d3.csv(`${base}/loc.csv`, row => ({
@@ -55,5 +69,24 @@ onMount(async () => {
 {#if languageData.length > 0}
 
 <BarHorizontal data={languageData} />
-
 {/if}
+<h3>Commits by time of day</h3>
+
+<svg viewBox="0 0 {width} {height}">
+    <g class="dots">
+    {#each commits as commit, index }
+        <circle
+            cx={ xScale(commit.datetime) }
+            cy={ yScale(commit.hourFrac) }
+            r="5"
+            fill="steelblue"
+        />
+    {/each}
+    </g>
+</svg>
+
+<style>
+    svg {
+        overflow: visible;
+    }
+</style>
