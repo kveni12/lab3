46948219e5d62f43a3bdc546f2f6caccccab0467
Krishna <kveni@mit.edu>
2026-03-19 20:17:02 -0400

    lab 7 - 1.5 (added axes)

--- src/routes/meta/+page.svelte
+++ src/routes/meta/+page.svelte
@@ -14,15 +14,33 @@ let width = 1000, height = 600;
 $: [minDate, maxDate] = d3.extent(commits.map(d => d.date));
 $: maxDatePlusOne = new Date(maxDate);
 $: maxDatePlusOne.setDate(maxDatePlusOne.getDate() + 1);
+let margin = { top: 20, right: 30, bottom: 40, left: 50 };
+
+let usableArea = {
+    top: margin.top,
+    right: width - margin.right,
+    bottom: height - margin.bottom,
+    left: margin.left
+};
+usableArea.width = usableArea.right - usableArea.left;
+usableArea.height = usableArea.bottom - usableArea.top;
 
 $: xScale = d3.scaleTime()
               .domain([minDate, maxDatePlusOne])
-              .range([0, width])
+              .range([usableArea.left, usableArea.right])
               .nice();
 
 $: yScale = d3.scaleLinear()
               .domain([24, 0])
-              .range([height, 0]);
+              .range([usableArea.bottom, usableArea.top]);
+let xAxis, yAxis;
+$: {
+    d3.select(xAxis).call(d3.axisBottom(xScale));
+    d3.select(yAxis).call(
+        d3.axisLeft(yScale)
+          .tickFormat(d => String(d % 24).padStart(2, "0") + ":00")
+    );
+}
 onMount(async () => {
 
     locData = await d3.csv(`${base}/loc.csv`, row => ({
@@ -73,6 +91,11 @@ onMount(async () => {
 <h3>Commits by time of day</h3>
 
 <svg viewBox="0 0 {width} {height}">
+    <!-- axes FIRST -->
+    <g transform="translate(0, {usableArea.bottom})" bind:this={xAxis} />
+    <g transform="translate({usableArea.left}, 0)" bind:this={yAxis} />
+
+    <!-- dots AFTER -->
     <g class="dots">
     {#each commits as commit, index }
         <circle
